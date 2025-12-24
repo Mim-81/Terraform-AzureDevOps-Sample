28c77eb40ad43e5e6936d17584a371530493a96e
Mima Maleki <mima.maleki@gmx.de>
2025-12-24 16:40:52 +0100

    first commit

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "azurerm" {
+<<<<<<< HEAD
+    resource_group_name  = "demo-resources"         # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "terraformmimademo1"     # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "prod-tfstate"           # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+=======
+    resource_group_name              = "demo-resources"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name             = "techtutorialmima"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name                   = "prod-tfstate"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                              = "prod.terraform.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+>>>>>>> 031e928f86c79e1012d18bfb426581ccc37da705
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  #default = "tfvmex"
+}
+
+variable "location" {
+<<<<<<< HEAD
+  #default = "West Europe"
+=======
+  #default = ""
+>>>>>>> 031e928f86c79e1012d18bfb426581ccc37da705
+}
+
+variable "subscription_id" {
+  type = string
+}
