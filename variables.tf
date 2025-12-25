47a40694c0d5318a66039e26798463addf79af3e
Mima Maleki <mima.maleki@gmx.de>
2025-12-25 17:22:43 +0100

    variables.tf update

--- variables.tf
+++ variables.tf
@@ -3,11 +3,7 @@ variable "prefix" {
 }
 
 variable "location" {
-<<<<<<< HEAD
   #default = "West Europe"
-=======
-  #default = ""
->>>>>>> 031e928f86c79e1012d18bfb426581ccc37da705
 }
 
 variable "subscription_id" {
